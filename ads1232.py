54aaf97f59c4990b6f0225b44a05f35e20f5f5e8
mansour9897 <mansour9897@gmail.com>
2023-07-03 08:18:46 +0330

    add def methods

--- ads1232.py
+++ ads1232.py
@@ -1,3 +1,4 @@
+from time import sleep
 class ADS1232:
     def __init__(self, pinData, pinSck, pinGain0, pinGain1, pinSpeed, pinPowerDown):
         self.__pinData = pinData
@@ -6,3 +7,14 @@ class ADS1232:
         self.__pinGain1 = pinGain1
         self.__pinSpeed = pinSpeed
         self.__pinPowerDown = pinPowerDown
+        
+        self.PowerReset()
+
+    def PowerReset(self):
+        sleep(1)
+
+    def ReadVoltage(self, channel):
+        raw = self.ReadRawValue(channel)
+
+    def ReadRawValue(self, channel):
+    
