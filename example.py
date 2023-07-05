21882ffe96dafa6724128670bcba809fb058bdd3
mansour9897 <mansour9897@gmail.com>
2023-07-05 17:26:42 +0330

    add rtd class

--- example.py
+++ example.py
@@ -1,17 +1,19 @@
 from time import sleep
 from ads1232 import ADS1232
-import RPi._GPIO as GPIO 
+from rtd import RTD
+import RPi._GPIO as GPIO
 
-ads = ADS1232(22,23,4,18,17,27,24)
+ads = ADS1232(22, 23, 4, 18, 17, 27, 24)
 ads.SetGain(128)
 ads.SetSpeed(80)
 
+pt100 = RTD(100, 66000, 5.0, ads)
 sleep(5)
 # ads = ADS1232(7,11,10,25,9,8,5)
 
 print("Welcome to Arianam MFI")
-while(True):
-    print(ads.ReadVoltage(0))
+while (True):
+    print(pt100.ReadTemperature())
     sleep(1)
 
 GPIO.cleanup()
